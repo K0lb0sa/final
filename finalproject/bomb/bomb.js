70e6debcb0b17a87a5550613862b61b65db6392d
K0lb0sa <86391922+K0lb0sa@users.noreply.github.com>
2022-02-02 18:49:15 +1000

    Add files via upload

---
+++ finalproject/bomb/bomb.js
@@ -0,0 +1,27 @@
+let timeout;
+let date;
+let date1  = new Date (0, 0, 0, 0, 0, 0, 0);
+
+function out() {
+    document.getElementById('timer').innerHTML = date.toLocaleTimeString();
+}
+async function timer() {
+    let time = new Promise((resolve, reject) => {
+        out();
+        if (date > date1) {
+            date.setSeconds(date.getSeconds()-1);
+            timeout = setTimeout(timer, 1000);
+        }
+        else {
+            resolve("бум");
+        }
+    })
+    .then(value => document.getElementById('timer').innerHTML = value);
+}
+
+function start(){
+    date = new Date(0, 0, 0, 0, 0, 5, 0);
+    clearTimeout(timeout);
+    out();
+    timer();
+}
